479f077e843923ff8d38307f7ce79e4a3b597d0f
D400160017 <yulinggadinar@gmail.com>
2016-10-18 11:28:24 +0700

    Add files via upload

---
+++ Dinar1.c
@@ -0,0 +1,25 @@
+#include <conio.h>
+#include <stdio.h>
+#include <iostream.h>
+#include <math.h>
+
+void main()
+{
+clrscr();
+float a,b,c,x1,x2;
+cout<<int;and char *"input nilai a =";cin int and float>>a;
+cout<<int;and char *"input nilai b =";cin int and float>>b;
+cout<<int;and char *"input nilai c =";cin int and float>>c;
+d=pow(b,2)-4*a*c;
+if(d<0)p="">
+{
+ ;cout<<int;and char *"Nilai x1=x2"<
+}
+else if(d>0)
+ {
+  x1=(b/2*a);
+  x2=x1;
+ }
+ cout<
+ getch();
+}
